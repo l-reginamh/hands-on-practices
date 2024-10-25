bc4c2b94097b9ee7423e6ebece66f118d242f3e1
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-25 10:25:32 +0800

    Update MessageConstants.java

--- auth.banking/src/main/java/com/simple/auth/banking/constants/MessageConstants.java
+++ auth.banking/src/main/java/com/simple/auth/banking/constants/MessageConstants.java
@@ -8,15 +8,18 @@ public class MessageConstants {
         throw new InvalidRequestException("Invalid request");
     }
     public static final String SUCCESS = "Success";
-    public static final String USER_CREATED = "User created successfully.";
-    public static final String USER_LOGGED_IN = "User logged in.";
     public static final String BAD_REQUEST = "Bad Request";
     public static final String VALIDATION_FAILED = "Validation failed";
-    public static final String INVALID_CREDENTIAL = "Invalid username or password.";
+    
+    public static final String USER_NOT_FOUND = "No user found!";
+    public static final String USER_ALREADY_EXIST = "Username taken, please try again.";
+    public static final String USER_CREATED = "User created successfully.";
+    public static final String USER_LOGGED_IN = "User logged in.";
     public static final String USER_LOCKED = "User has been locked, please contact customer service.";
     public static final String USER_CLOSED = "This User is no longer available.";
-    public static final String USER_ALREADY_EXIST = "Username taken, please try again.";
-    public static final String USER_NOT_FOUND = "No user found!";
+    
+    public static final String INVALID_CREDENTIAL = "Invalid username or password.";
+    
     public static final String ACCOUNT_NOT_FOUND = "No account found!";
     public static final String CONTACT_NOT_FOUND = "No contact found!";
     public static final String TRANSACTION_NOT_FOUND = "No transaction found!";
