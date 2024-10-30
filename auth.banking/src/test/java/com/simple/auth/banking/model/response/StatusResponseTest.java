5932770ef50db86b49a860680aa82ab29c0ba844
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:03:35 +0800

    Create StatusResponseTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/response/StatusResponseTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.response;
+
+@ExtendWith(MockitoExtension.class)
+class StatusResponseTest {
+    @InjectMocks
+    private StatusResponse statusResponse;
+
+    @Test
+    void statusResponseTest() {
+
+    }
+}
