94407a5430d944e7cefcba6e21d16920411c99ee
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:34:12 +0800

    Create AccountTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/entity/AccountTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.entity;
+
+@ExtendWith(MockitoExtension.class)
+class AccountTest {
+    @InjectMocks
+    private Account account;
+
+    @Test
+    void accountTest() {
+
+    }
+}
