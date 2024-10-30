d4036dd37e4f46d82e599194ee64067e628865e8
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:00:53 +0800

    Create ApiResponseTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/response/ApiResponseTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.response;
+
+@ExtendWith(MockitoExtension.class)
+class ApiResponseTest {
+    @InjectMocks
+    private ApiResponse apiResponse;
+
+    @Test
+    void apiResponseTest() {
+
+    }
+}
