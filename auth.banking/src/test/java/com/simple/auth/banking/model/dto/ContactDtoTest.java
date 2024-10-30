db2b2b1e77bb6fa4869f9b1b2da9d6d14df205c6
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:23:34 +0800

    Create ContactDtoTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/ContactDtoTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.dto;
+
+@ExtendWith(MockitoExtension.class)
+class ContactDtoTest {
+    @InjectMocks
+    private ContactDto contactDto;
+
+    @Test
+    void contactDtoTest() {
+
+    }
+}
