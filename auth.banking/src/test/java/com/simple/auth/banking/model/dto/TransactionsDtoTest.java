4555b689a1396684bbe6a640696ff93e41f89d77
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:33:00 +0800

    Create TransactionsDtoTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/TransactionsDtoTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.dto;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionsDtoTest {
+    @InjectMocks
+    private TransactionsDto transactionsDto;
+
+    @Test
+    void transactionsDtoTest() {
+
+    }
+}
