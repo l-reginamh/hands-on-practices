41d537ddfbb70e128d1308cb57ae363ef2817253
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 13:27:30 +0800

    Update AccountControllerTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/controller/AccountControllerTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/controller/AccountControllerTest.java
@@ -9,7 +9,92 @@ class AccountControllerTest {
     private AccountService accountService;
 
     @Test
-    void () {
+    void getAllAccounts_resultDataFoundException() {
+        
+    }
+
+    @Test
+    void getAllAccounts_resultException() {
+        
+    }
+
+    @Test
+    void getAllAccounts_resultSuccess() {
+        
+    }
+
+    @Test
+    void getAccount_resultDataFoundException() {
+        
+    }
+
+    @Test
+    void getAccount_resultException() {
+        
+    }
+
+    @Test
+    void getAccount_resultSuccess() {
+        
+    }
+
+    @Test
+    void createAccount_resultAlreadyExistsException() {
+        
+    }
+
+    @Test
+    void createAccount_resultException() {
+        
+    }
+
+    @Test
+    void createAccount_resultSuccess() {
+        
+    }
+
+    @Test
+    void updateAccount_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void updateAccount_resultException() {
+        
+    }
+
+    @Test
+    void updateAccount_resultSuccess() {
+        
+    }
+
+    @Test
+    void activateAccount_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void activateAccount_resultException() {
+        
+    }
+
+    @Test
+    void activateAccount_resultSuccess() {
+        
+    }
+
+    @Test
+    void deactivateAccount_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void deactivateAccount_resultException() {
+        
+    }
+
+    @Test
+    void deactivateAccount_resultSuccess() {
         
     }
 }
