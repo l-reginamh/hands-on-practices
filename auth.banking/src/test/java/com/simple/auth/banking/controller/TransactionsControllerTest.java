932f6867b309d52f950c9c0871fd390cde173357
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:14:03 +0800

    Create TransactionsControllerTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/controller/TransactionsControllerTest.java
@@ -0,0 +1,70 @@
+package com.simple.auth.banking.controller;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionsControllerTest {
+    @InjectMocks
+    private TransactionsController transactionsController
+
+    @Mock
+    private TransactionsService transactionsService;
+
+    @Test
+    void getAllTransactions_resultDataFoundException() {
+        
+    }
+
+    @Test
+    void getAllTransactions_resultException() {
+        
+    }
+
+    @Test
+    void getAllTransactions_resultSuccess() {
+        
+    }
+
+    @Test
+    void getTransaction_resultDataFoundException() {
+        
+    }
+
+    @Test
+    void getTransaction_resultException() {
+        
+    }
+
+    @Test
+    void getTransaction_resultSuccess() {
+        
+    }
+
+    @Test
+    void createTransaction_resultAlreadyExistsException() {
+        
+    }
+
+    @Test
+    void createTransaction_resultException() {
+        
+    }
+
+    @Test
+    void createTransaction_resultSuccess() {
+        
+    }
+
+    @Test
+    void updateTransaction_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void updateTransaction_resultException() {
+        
+    }
+
+    @Test
+    void updateTransaction_resultSuccess() {
+        
+    }
+}
