dcfebdd29b46cbd61e8fc51fe7ea5633659582f1
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:38:26 +0800

    Create ServiceUserTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/entity/ServiceUserTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.entity;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceUserTest {
+    @InjectMocks
+    private ServiceUser serviceUser;
+
+    @Test
+    void serviceUserTest() {
+
+    }
+}
