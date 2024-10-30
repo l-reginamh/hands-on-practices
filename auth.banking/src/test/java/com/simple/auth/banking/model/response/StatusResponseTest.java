beb61ac29a26de30fc50e62c55c3e6c4a89eef77
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:09:38 +0800

    Update StatusResponseTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/response/StatusResponseTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/response/StatusResponseTest.java
@@ -7,6 +7,13 @@ class StatusResponseTest {
 
     @Test
     void statusResponseTest() {
+        statusResponse = new StatusResponse();
+        statusResponse.setCode("200");
+        statusResponse.setDescription("Success");
+        statusResponse.setMessage("Request processed successfully.");
 
+        assertEquals("200", statusResponse.getCode());
+        assertEquals("Success", statusResponse.getDescription());
+        assertEquals("Request processed successfully.", statusResponse.getMessage());
     }
 }
