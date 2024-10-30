eb58b48dd18f86bbccbef083980b19840c39e822
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:47:47 +0800

    Create ContactRequestTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/request/ContactRequestTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.request;
+
+@ExtendWith(MockitoExtension.class)
+class ContactRequestTest {
+    @InjectMocks
+    private ContactRequest contactRequest;
+
+    @Test
+    void contactRequestTest() {
+
+    }
+}
