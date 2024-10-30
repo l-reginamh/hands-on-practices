07adbcb0521417babfb9be2739f7c8c7f6015878
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:30:06 +0800

    Create ServiceUserDto.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/ServiceUserDto.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.dto;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceUserDtoTest {
+    @InjectMocks
+    private ServiceUserDto serviceUserDto;
+
+    @Test
+    void serviceUserDtoTest() {
+
+    }
+}
