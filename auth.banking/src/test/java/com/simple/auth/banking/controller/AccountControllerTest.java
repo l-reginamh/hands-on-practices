f59fa3dc06eec721d13496a7696b61ca9dd3f60e
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 13:21:29 +0800

    Create AccountControllerTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/controller/AccountControllerTest.java
@@ -0,0 +1,15 @@
+package com.simple.auth.banking.controller;
+
+@ExtendWith(MockitoExtension.class)
+class AccountControllerTest {
+    @InjectMocks
+    private AccountController accountController
+
+    @Mock
+    private AccountService accountService;
+
+    @Test
+    void () {
+        
+    }
+}
