99993d5d56b2dc09351de8e84f308497b4b36e79
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:59:15 +0800

    Create TransactionsRequestTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/request/TransactionsRequestTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.request;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionsRequestTest {
+    @InjectMocks
+    private TransactionsRequest transactionsRequest;
+
+    @Test
+    void transactionsRequestTest() {
+
+    }
+}
