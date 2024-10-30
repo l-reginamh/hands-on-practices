3c1dfaac56ec11f8422c0a287e20d955989d2d77
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:18:23 +0800

    Update ApiResponseTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/response/ApiResponseTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/response/ApiResponseTest.java
@@ -7,6 +7,13 @@ class ApiResponseTest {
 
     @Test
     void apiResponseTest() {
+        apiResponse = new ApiResponse;
+        StatusResponse statusResponse = new StatusResponse();
+        statusResponse.setCode("200");
+        apiResponse.setStatusResponse(statusResponse);
+        apiResponse.setData("sample data");
 
+        assertEquals("200", apiResponse.getStatusResponse().getCode());
+        assertEquals("sample data", apiResponse.getData());
     }
 }
