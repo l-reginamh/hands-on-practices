66d23648c215478bbb302c2a0c5405b895d1715b
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:45:22 +0800

    Update ContactRequestTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/request/ContactRequestTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/request/ContactRequestTest.java
@@ -7,6 +7,16 @@ class ContactRequestTest {
 
     @Test
     void contactRequestTest() {
+        contactRequest.setAccountNo(10000001L);
+        contactRequest.setCustomerId("910101101001");
+        contactRequest.setMobile("0198273829");
+        contactRequest.setAddress("No 1, Jalan Malaysia");
+        contactRequest.setEmail("testing@gmail.com");
 
+        assertEquals(10000001L, contactRequest.getAccountNo());
+        assertEquals("910101101001", contactRequest.getCustomerId());
+        assertEquals("0198273829", contactRequest.getMobile());
+        assertEquals("No 1, Jalan Malaysia", contactRequest.getAddress());
+        assertEquals("testing@gmail.com", contactRequest.getEmail());
     }
 }
