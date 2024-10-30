b4ae93f2de2badba159761d62f8053c5c418544e
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 17:35:08 +0800

    Update ServiceUserDtoTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/dto/ServiceUserDtoTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/ServiceUserDtoTest.java
@@ -7,6 +7,19 @@ class ServiceUserDtoTest {
 
     @Test
     void serviceUserDtoTest() {
+        serviceUserDto = new ServiceUserDto();
+        serviceUserDto.setId(1L);
+        serviceUserDto.setUsername("username");
+        serviceUserDto.setPassword("password");
+        serviceUserDto.setCustomerId("910101101001");
+        serviceUserDto.setUserStatus(UserStatus.ACTIVE);
+        serviceUserDto.setRole(UserRole.PERSONAL);
 
+        assertEquals(1L, serviceUserDto.getId());
+        assertEquals("username", serviceUserDto.getUsername());
+        assertEquals("password", serviceUserDto.getPassword());
+        assertEquals("910101101001", serviceUserDto.getCustomerId());
+        assertEquals(UserStatus.ACTIVE, serviceUserDto.getUserStatus());
+        assertEquals(UserRole.PERSONAL, serviceUserDto.getRole());
     }
 }
