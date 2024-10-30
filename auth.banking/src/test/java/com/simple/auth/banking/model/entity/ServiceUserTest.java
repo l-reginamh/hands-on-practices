2dd2b394f134abfb41c1b37677c5570d590012f2
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 16:54:20 +0800

    Update ServiceUserTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/entity/ServiceUserTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/entity/ServiceUserTest.java
@@ -7,6 +7,22 @@ class ServiceUserTest {
 
     @Test
     void serviceUserTest() {
+        serviceUser.setId(1L);
+        serviceUser.setUsername("username");
+        serviceUser.setPassword("password");
+        serviceUser.setCustomerId("910101101001");
+        serviceUser.setUserStatus(UserStatus.ACTIVE);
+        serviceUser.setRole(UserRole.PERSONAL);
+        serviceUser.setCreatedDate(new Date(Calendar.getInstance().getTimeInMillis()));
+        serviceUser.setUpdatedDate(new Date(Calendar.getInstance().getTimeInMillis()));
 
+        assertEquals(1L, serviceUser.getId());
+        assertEquals("username", serviceUser.getUsername());
+        assertEquals("password", serviceUser.getPassword());
+        assertEquals("910101101001", serviceUser.getCustomerId());
+        assertEquals(UserStatus.ACTIVE, serviceUser.getUserStatus());
+        assertEquals(UserRole.PERSONAL, serviceUser.getRole());
+        assertNotNull(serviceUser.getCreatedDate());
+        assertNotNull(serviceUser.getUpdatedDate());
     }
 }
