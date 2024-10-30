367242b0c4fe8d63c4e8c4d8cee5759f9194d761
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:18:47 +0800

    Update ApiTokenResponseTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/response/ApiTokenResponseTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/response/ApiTokenResponseTest.java
@@ -10,7 +10,7 @@ class ApiTokenResponseTest {
         apiTokenResponse = new ApiTokenResponse;
         StatusResponse statusResponse = new StatusResponse();
         statusResponse.setCode("200");
-        apiTokenResponse.setStatusResponse(new StatusResponse());
+        apiTokenResponse.setStatusResponse(statusResponse);
         apiTokenResponse.setToken("d9298r3y9r32e3yhd9fd920ujhdnc20-1iej0n2d0d");
         apiTokenResponse.setData("sample data");
 
