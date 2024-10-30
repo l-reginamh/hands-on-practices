5c8179c9500315a1031840bcc3d4837bfb70f33f
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:48:48 +0800

    Create LoginRequestTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/request/LoginRequestTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.request;
+
+@ExtendWith(MockitoExtension.class)
+class LoginRequestTest {
+    @InjectMocks
+    private LoginRequest loginRequest;
+
+    @Test
+    void loginRequestTest() {
+
+    }
+}
