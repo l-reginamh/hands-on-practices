e18abef3c6eef23361a79660062a65d574679751
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 13:36:24 +0800

    Create ContactControllerTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/controller/ContactControllerTest.java
@@ -0,0 +1,55 @@
+package com.simple.auth.banking.controller;
+
+@ExtendWith(MockitoExtension.class)
+class ContactControllerTest {
+    @InjectMocks
+    private ContactController contactController
+
+    @Mock
+    private ContactService contactService;
+
+    @Test
+    void getContact_resultDataFoundException() {
+        
+    }
+
+    @Test
+    void getContact_resultException() {
+        
+    }
+
+    @Test
+    void getContact_resultSuccess() {
+        
+    }
+  
+    @Test
+    void createContact_resultAlreadyExistsException() {
+        
+    }
+
+    @Test
+    void createContact_resultException() {
+        
+    }
+
+    @Test
+    void createContact_resultSuccess() {
+        
+    }
+
+    @Test
+    void updateContact_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void updateContact_resultException() {
+        
+    }
+
+    @Test
+    void updateContact_resultSuccess() {
+        
+    }
+}
