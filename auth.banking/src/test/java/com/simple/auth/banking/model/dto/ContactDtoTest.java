c762d981e0bad86c5b2a1dc0f755c005d0e456d5
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 17:33:13 +0800

    Update ContactDtoTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/dto/ContactDtoTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/ContactDtoTest.java
@@ -7,6 +7,19 @@ class ContactDtoTest {
 
     @Test
     void contactDtoTest() {
+        contactDto = new ContactDto();
+        contactDto.setId(1L);
+        contactDto.setAccountNo(10000001L);
+        contactDto.setCustomerId("910101101001");
+        contactDto.setMobile("0191234567");
+        contactDto.setAddress("No 1, Jalan Malaysia");
+        contactDto.setEmail("testing@gmail.com");
 
+        assertEqual(1L, contactDto.getId());
+        assertEqual(10000001L, contactDto.getAccountNo());
+        assertEqual("910101101001", contactDto.getCustomerId());
+        assertEqual(0191234567, contactDto.getMobile());
+        assertEqual("No 1, Jalan Malaysia", contactDto.getAddress());
+        assertEqual("testing@gmail.com", contactDto.getEmail());
     }
 }
