21547cd3e6cd48bee117ada799e79f8ac3682c16
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 14:22:20 +0800

    Create AccountDtoTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/model/dto/AccountDtoTest.java
@@ -0,0 +1,12 @@
+package com.simple.auth.banking.model.dto;
+
+@ExtendWith(MockitoExtension.class)
+class AccountDtoTest {
+    @InjectMocks
+    private Accountdto accountDto;
+
+    @Test
+    void accountDtoTest() {
+
+    }
+}
