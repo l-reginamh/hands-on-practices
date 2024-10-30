cb4c9e5c4c21a37071b302cb2c8395350a88f761
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 15:40:55 +0800

    Update AccountRequestTest.java

--- auth.banking/src/test/java/com/simple/auth/banking/model/request/AccountRequestTest.java
+++ auth.banking/src/test/java/com/simple/auth/banking/model/request/AccountRequestTest.java
@@ -7,6 +7,29 @@ class AccountRequestTest {
 
     @Test
     void accountRequestTest() {
+        accountRequest = new AccountRequest();
 
+        accountRequest.setAccountNo(10000001L);
+        accountRequest.setAccountType(AccountType.SAVINGS);
+        accountRequest.setName("Jane Doe");
+        accountRequest.setCustomerId("910101101001");
+        accountRequest.setAccountStatus(AccountStatus.ACTIVE);
+        accountRequest.setTransactStatus(TransactStatus.ACTIVE);
+        accountRequest.setTransactionLimit(new BigDecimal("3000.00"));
+        accountRequest.setCardNo("1234123412341234");
+        accountRequest.setCardStatus(CardStatus.ACTIVE);
+        accountRequest.setCardExpiry("01/28");
+        accountRequest.setCardNo("************1234");
+
+        assertEquals(10000001L, accountRequest.getAccountNo());
+        assertEquals(AccountType.SAVINGS, accountRequest.getAccountType());
+        assertEquals("Jane Doe", accountRequest.getName());
+        assertEquals("910101101001", accountRequest.getCustomerId());
+        assertEquals(AccountType.ACTIVE, accountRequest.getAccountStatus());
+        assertEquals(AccountType.ACTIVE, accountRequest.getTransactStatus());
+        assertEquals(new BigDecimal("3000.00"), accountRequest.getTransactionLimit());
+        assertEquals(CardStatus.ACTIVE, accountRequest.getCardStatus());
+        assertEquals("01/28", accountRequest.getCardExpiry());
+        assertEquals("************1234", accountRequest.getCardNo());
     }
 }
