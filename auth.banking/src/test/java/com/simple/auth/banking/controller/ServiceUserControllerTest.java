19432d7f54bf685f79ceb8a599ab7365d145f1b4
Regina <115763453+l-reginamh@users.noreply.github.com>
2024-10-30 13:46:43 +0800

    Create ServiceUserControllerTest.java

---
+++ auth.banking/src/test/java/com/simple/auth/banking/controller/ServiceUserControllerTest.java
@@ -0,0 +1,65 @@
+package com.simple.auth.banking.controller;
+
+@ExtendWith(MockitoExtension.class)
+class ServiceUserControllerTest {
+    @InjectMocks
+    private ServiceUserController serviceUserController
+
+    @Mock
+    private ServiceUserService serviceUserService;
+
+    @Test
+    void updateUser_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void updateUser_resultException() {
+        
+    }
+
+    @Test
+    void updateUser_resultSuccess() {
+        
+    }
+
+    @Test
+    void activateUser_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void activateUser_resultInvalidRequestException() {
+        
+    }
+
+    @Test
+    void activateUser_resultException() {
+        
+    }
+
+    @Test
+    void activateUser_resultSuccess() {
+        
+    }
+
+    @Test
+    void deactivateUser_resultDataNotFoundException() {
+        
+    }
+
+    @Test
+    void deactivateUser_resultInvalidRequestException() {
+        
+    }
+
+    @Test
+    void deactivateUser_resultException() {
+        
+    }
+
+    @Test
+    void deactivateUser_resultSuccess() {
+        
+    }
+}
